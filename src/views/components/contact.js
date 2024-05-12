4ae45981ca7508e2620347fd755b7c77ef0e0027
Anujj <anujjbubna39@gmail.com>
2024-05-12 12:05:41 -0700

    made features responsive to small screen size

--- src/views/components/contact.js
+++ src/views/components/contact.js
@@ -1,60 +1,89 @@
 import React from "react";
 
 class Contact extends React.Component {
-    render() {
-        return (
-            <div id="contact" className="box-border w-screen h-screen text-olive-green bg-pastel-green snap-start shrink-0 snap-always">
-                <div className="w-full h-full lg:pt-28 pt-20">
-                    <h1 className="text-4xl lg:text-5xl xl:text-6xl text-center lg:pb-8 pb-6 2xl:pb-2 text-olive-green drop-shadow-xl lg:pt-0 px-4">Contact Us</h1>
-                    <form className=" bg-gray-50 mx-auto 2xl:mt-8 xl:mt-10 md:mt-6 mt-4 pt-4 2xl:pt-4 xl:pt-10 h-3/5 w-1/2 
+  render() {
+    return (
+      <div
+        id="contact"
+        className="box-border w-screen h-screen text-olive-green bg-pastel-green snap-start shrink-0 snap-always"
+      >
+        <div className="w-full h-full lg:pt-28 pt-20">
+          <h1 className="text-4xl lg:text-5xl xl:text-6xl text-center lg:pb-8 pb-6 2xl:pb-2 text-olive-green drop-shadow-xl lg:pt-0 px-4">
+            Contact Us
+          </h1>
+          <form
+            className=" bg-gray-50 mx-auto 2xl:mt-8 xl:mt-10 md:mt-6 mt-4 pt-4 2xl:pt-4 xl:pt-10 h-3/5 w-1/2 
                                     rounded-lg drop-shadow-xl"
-                        action="https://formspree.io/f/xleqvbrd" method="POST">
-                        <div className="grid grid-cols-2 mx-auto w-1/2 mt-8 2xl:mt-0 text-center">
-                            <div className="m-auto w-4/5 ">
-                                <label for="fname" className="w-full my-1 font-medium">First Name
-                                    <input className="rounded-md w-5/6 2xl:w-full mt-3 2xl:mt-1 mb-1 px-2 py-1 text-sm text-center shadow-lg
+            action="https://formspree.io/f/xleqvbrd"
+            method="POST"
+          >
+            <div className="grid grid-cols-2 mx-auto lg:w-1/2 w-5/6 mt-14 xl:mt-8 lg:mt-2 2xl:mt-0 text-center">
+              <div className="m-auto w-4/5 ">
+                <label for="fname" className="w-full my-1 font-medium">
+                  First Name
+                  <input
+                    className="rounded-md w-5/6 2xl:w-full mt-3 2xl:mt-1 mb-1 px-2 py-1 text-sm text-center shadow-lg
                                                                 border-olive-green border-solid border-2"
-                                        type="text" id="fname" name="fname"
-                                        placeholder="First name">
-                                    </input>
-                                </label>
-                                {/* add input reference and create js script */}
-                            </div>
-                            <div className="m-auto w-4/5">
-                                <label for="lname" className="w-full my-1 font-medium">Last Name</label>
-                                <input className="rounded-md w-5/6 2xl:w-full mt-3 2xl:mt-1 mb-1 px-2 py-1 text-sm text-center shadow-lg
+                    type="text"
+                    id="fname"
+                    name="fname"
+                    placeholder="First name"
+                  ></input>
+                </label>
+                {/* add input reference and create js script */}
+              </div>
+              <div className="m-auto w-4/5">
+                <label for="lname" className="w-full my-1 font-medium">
+                  Last Name
+                  <input
+                    className="rounded-md w-5/6 2xl:w-full mt-3 2xl:mt-1 mb-1 px-2 py-1 text-sm text-center shadow-lg
                                                                 border-olive-green border-solid border-2"
-                                    type="text" id="lname" name="lname"
-                                    placeholder="Last name">
-                                </input>
-                            </div>
-                        </div>
-                        <div className="w-2/3 m-auto mt-4 2xl:mt-2 grid grid-flow-row auto-rows-auto ">
-                            <label for="email" className="w-4/5 mx-auto my-1 font-medium text-center">Email address
-                                <input className="rounded-md w-5/6 mt-3 2xl:mt-1 mb-1 px-2 py-1 text-sm text-center shadow-lg
+                    type="text"
+                    id="lname"
+                    name="lname"
+                    placeholder="Last name"
+                  ></input>
+                </label>
+              </div>
+            </div>
+            <div className="w-3/4 lg:w-2/3 px-2 lg:px-0 m-auto mt-4 xl:mt-4 lg:mt-2 2xl:mt-2 grid grid-flow-row auto-rows-auto ">
+              <label
+                for="email"
+                className="w-full lg:w-4/5 mx-auto my-1 font-medium text-center"
+              >
+                Email address
+                <input
+                  className="rounded-md w-full lg:w-5/6 mt-3 2xl:mt-1 mb-1 px-2 py-1 text-sm text-center shadow-lg
                                                                 border-olive-green border-solid border-2"
-                                    type="email" id="email" name="email"
-                                    placeholder="Email">
-                                </input>
-                            </label>
-                            {/* check for email input only */}
-                        </div>
-                        <div className="m-auto w-2/3 grid grid-flow-row auto-rows-auto justify-items-center mt-4 2xl:mt-2">
-                            <label for="query" className="w-4/5 mt-1 mb-2 font-medium text-center">Your Query:
-                                <textarea className="rounded-md w-5/6 h-32 mt-3 2xl:mt-1 mb-1 px-2 py-1 text-sm shadow-lg resize-none
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                ></input>
+              </label>
+              {/* check for email input only */}
+            </div>
+            <div className="m-auto w-3/4 lg:w-2/3 px-2 lg:px-0 grid grid-flow-row auto-rows-auto justify-items-center mt-4 xl:mt-4 lg:mt-2 2xl:mt-2">
+              <label
+                for="query"
+                className="w-full lg:w-4/5 mt-1 mb-2 font-medium text-center"
+              >
+                Your Query:
+                <textarea
+                  className="rounded-md w-full lg:w-5/6 xl:h-32 h-24 mt-3 2xl:mt-1 mb-1 px-2 py-1 text-sm shadow-lg resize-none
                                                                 border-olive-green border-solid border-2"
-                                    type="text" id="query" name="query"
-                                    placeholder="Ask us your questions...">
-                                </textarea>
-                            </label>
-                            {/* limit the query to 60 words */}
-                        </div>
-                        <div className="w-16 m-auto bg-olive-green mt-6 2xl:mt-0 text-white rounded-md px-1 text-center">
-                            <button>
-                                Submit
-                            </button>
-                        </div>
-                        {/* <form className="pt-10 m-auto w-1/3 space-y-8" action="#" method="post">
+                  type="text"
+                  id="query"
+                  name="query"
+                  placeholder="Ask us your questions..."
+                ></textarea>
+              </label>
+              {/* limit the query to 60 words */}
+            </div>
+            <div className="w-16 m-auto bg-olive-green xl:mt-6 lg:mt-0 2xl:mt-0 text-white rounded-md px-1 text-center">
+              <button>Submit</button>
+            </div>
+            {/* <form className="pt-10 m-auto w-1/3 space-y-8" action="#" method="post">
                             <label className="" for="name">Name:</label>
                             <input type="text" id="name" name="name" required
                                 className="pl-3 py-1 mx-2 rounded-lg border-slate-300"></input>
@@ -69,20 +98,27 @@ class Contact extends React.Component {
                             <br></br>
                             <input type="submit" value="Submit"></input>
                         </form> */}
-                    </form>
-                    <div className="flex justify-center w-3/10 pt-16
-                                     text-xl hover:text-2xl hover:ease-in-out duration-300">
-                        <p className="float-left pl-4 py-1 text-olive-green font-medium drop-shadow-xl">Interested parents and caregivers, waitlist today!</p>
-                        <a href="https://shorturl.at/lpzX1" target="_blank"><button className="bg-olive-green float-right px-3 ml-4 py-1
-                                        rounded drop-shadow-xl text-white">
-                            Register
-                        </button></a>
-                    </div>
-                </div>
-            </div>
-
-        )
-    }
+          </form>
+          <div
+            className="flex justify-center w-3/10 pt-16
+                                     text-xl hover:text-2xl hover:ease-in-out duration-300"
+          >
+            <p className="float-left pl-4 py-1 text-olive-green font-medium drop-shadow-xl">
+              Interested parents and caregivers, waitlist today!
+            </p>
+            <a href="https://shorturl.at/lpzX1" target="_blank">
+              <button
+                className="bg-olive-green float-right px-3 ml-4 py-1
+                                        rounded drop-shadow-xl text-white"
+              >
+                Register
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Contact;
--- src/views/components/features.js
+++ src/views/components/features.js
@@ -7,97 +7,108 @@ class Features extends React.Component {
         id="features"
         className="box-border w-screen h-screen text-white bg-baby-blue snap-start shrink-0 snap-always"
       >
-        <div className="w-full h-full lg:pt-28 pt-20">
-          <h1 className="text-4xl lg:text-5xl xl:text-6xl text-center lg:pb-8 pb-6 2xl:pb-2 text-white drop-shadow-xl lg:pt-0 px-4">
+        <div className="w-full h-full xl:pt-28 pt-20">
+          <h1 className="text-4xl lg:text-5xl xl:text-6xl text-center xl:pb-8 pb-2 2xl:pb-2 text-white drop-shadow-xl lg:pt-0 px-4">
             Perks we offer
           </h1>
           <p className="w-full pt-0 lg:pt-2 xl:pt-4 2xl:pt-2 px-2 text-center italic drop-shadow-xl text-white lg:text-base text-sm">
             Other than chocolates, of course :P
           </p>
-          <div className="grid justify-items-center grid-cols-2 auto-cols-auto ml-8 mr-14 pt-4 xl:pt-10 h-3/5">
+          <div className="grid justify-items-center grid-cols-2 auto-cols-auto sm:ml-8 ml-10 sm:mr-14 mr-10 pt-4 xl:pt-10 h-3/5">
             <div
-              className="grid justify-items-center grid-cols-2 ml-8 mr-4 rounded-xl text-slate-600
-                                       border-2 border-slate-400 bg-slate-100 drop-shadow-md
-                                       hover:drop-shadow-xl h-5/6"
+              className="grid justify-items-center grid-cols-1 lg:grid-cols-2 grid-flow-row lg:grid-rows-1 sm:ml-8 sm:mr-4 rounded-xl text-slate-600
+                                       border-2 border-slate-400 bg-slate-100 drop-shadow-md auto-rows-auto
+                                       hover:drop-shadow-xl xl:h-5/6 md:h-full h-5/6 sm:h-2/3 sm:pb-48 sm:mt-4 md:pb-0 pb-44"
             >
-              <img
-                alt="image"
-                src="/images/parentchild.jpg"
-                className="w-full h-5/6 rounded-l-lg object-cover"
-              />
-              <div className="text-xl 2xl:text-xl px-5 text-left flex flex-col pb-8">
-                <span className="text-3xl 2xl:text-2xl mb-3 2xl:mb-0 pt-12 2xl:pt-8 pb-8 2xl:pb-4 text-center">
+              <div className="lg:h-full sm:h-full w-full">
+                <img
+                  alt="image"
+                  src="/images/parentchild.jpg"
+                  className="w-full h-36 lg:h-full lg:rounded-l-lg lg:rounded-r-none rounded-t-lg object-cover"
+                />
+              </div>
+
+              <div className="text-sm sm:text-xl lg:text-sm xl:text-xl px-2 sm:px-5 text-left flex flex-col lg:pb-8 pb-0  ">
+                <span className="text-lg md:text-3xl sm:text-2xl lg:text-xl 2xl:text-2xl mb-3 2xl:mb-0 pt-4 sm:pt-6 lg:pt-8 xl:pt-12 2xl:pt-8 md:pb-8 pb-0 2xl:pb-4 xl:pb-8 lg:pb-6 text-center ">
                   For Parents & Children
                 </span>
-                <span className="grid grid-flow-col auto-cols-auto">
-                  <img
-                    src="/images/bricks.png"
-                    className="h-6 w-6 ml-1 mr-4 mt-3"
-                  ></img>
-                  <span className="">
-                    Standard government inspection for caregivers
+                <div className="h-full">
+                  <span className="grid grid-flow-col auto-cols-auto sm:mb-0 mb-2">
+                    <img
+                      src="/images/bricks.png"
+                      className="h-6 w-6 ml-1 mr-4 mt-3"
+                    ></img>
+                    <span className="">
+                      Standard government inspection for caregivers
+                    </span>
+                    <br></br>
                   </span>
-                  <br></br>
-                </span>
-                <span className="grid grid-flow-col auto-cols-auto">
-                  <img
-                    src="/images/bricks.png"
-                    className="h-6 w-6 ml-1 mr-4 mt-1"
-                  ></img>
-                  <span className="pr-1">Meet the caregiver before</span>
-                  <br></br>
-                </span>
-                <span className="grid grid-flow-col auto-cols-auto">
-                  <img
-                    src="/images/bricks.png"
-                    className="h-6 w-6 ml-1 mr-4 mt-3"
-                  ></img>
-                  <span className="pr-1">
-                    Filters include location, price, and ratings{" "}
+                  <span className="grid grid-flow-col auto-cols-auto sm:mb-0 mb-2">
+                    <img
+                      src="/images/bricks.png"
+                      className="h-6 w-6 ml-1 mr-4 mt-1"
+                    ></img>
+                    <span className="pr-1">Meet the caregiver before</span>
+                    <br></br>
                   </span>
-                  <br></br>
-                </span>
-                <span className="grid grid-flow-col auto-cols-auto">
-                  <img
-                    src="/images/bricks.png"
-                    className="h-6 w-6 ml-1 mr-4 mt-3"
-                  ></img>
-                  <span className="pr-1">Seemless appointment booking</span>
-                  <br></br>
-                </span>
-                <span className="grid grid-flow-col auto-cols-auto">
-                  <img
-                    src="/images/bricks.png"
-                    className="h-6 w-6 ml-1 mr-4 mt-3"
-                  ></img>
-                  <span className="pr-1">Daily, weekly, or monthly plans</span>
-                  <br></br>
-                </span>
-                <span className="grid grid-flow-col auto-cols-auto">
-                  <img
-                    src="/images/bricks.png"
-                    className="h-6 w-6 ml-1 mr-4 mt-3"
-                  ></img>
-                  <span className="pr-1">Integrated payments system</span>
-                  <br></br>
-                </span>
+                  <span className="grid grid-flow-col auto-cols-auto sm:mb-0 mb-2">
+                    <img
+                      src="/images/bricks.png"
+                      className="h-6 w-6 ml-1 mr-4 mt-3"
+                    ></img>
+                    <span className="pr-1">
+                      Filters include location, price, and ratings
+                    </span>
+                    <br></br>
+                  </span>
+                  <span className="grid grid-flow-col auto-cols-auto sm:mb-0 mb-2">
+                    <img
+                      src="/images/bricks.png"
+                      className="h-6 w-6 ml-1 mr-4 mt-3"
+                    ></img>
+                    <span className="pr-1">Seemless appointment booking</span>
+                    <br></br>
+                  </span>
+                  <span className="grid grid-flow-col auto-cols-auto sm:mb-0 mb-2">
+                    <img
+                      src="/images/bricks.png"
+                      className="h-6 w-6 ml-1 mr-4 mt-3"
+                    ></img>
+                    <span className="pr-1">
+                      Daily, weekly, or monthly plans
+                    </span>
+                    <br></br>
+                  </span>
+                  <span className="grid grid-flow-col auto-cols-auto sm:mb-0 mb-2s">
+                    <img
+                      src="/images/bricks.png"
+                      className="h-6 w-6 ml-1 mr-4 mt-3"
+                    ></img>
+                    <span className="pr-1 sm:mb-4">
+                      Integrated payments system
+                    </span>
+                    <br></br>
+                  </span>
+                </div>
               </div>
             </div>
             <div
-              className="grid justify-items-center grid-cols-2 ml-8 mr-4 rounded-xl text-slate-600
-                                       border-2 border-slate-400 bg-slate-100 drop-shadow-md
-                                       hover:drop-shadow-xl h-5/6"
+              className="grid justify-items-center grid-cols-1 lg:grid-cols-2 grid-flow-row lg:grid-rows-1 sm:ml-4 sm:mr-8 rounded-xl text-slate-600
+              border-2 border-slate-400 bg-slate-100 drop-shadow-md auto-rows-auto
+              hover:drop-shadow-xl xl:h-5/6 md:h-full h-5/6 sm:h-2/3 sm:pb-48 sm:mt-4 md:pb-0 pb-44"
             >
-              <img
-                alt="image"
-                src="/images/provider.jpg"
-                className="w-full h-full rounded-l-lg object-cover"
-              />
-              <div className="text-xl 2xl:text-xl px-5 text-left flex flex-col pb-8">
-                <span className="text-3xl 2xl:text-2xl mb-3 2xl:mb-0 pt-12 2xl:pt-8 pb-8 2xl:pb-4 text-center">
-                  For Caregiver
+              <div className="lg:h-full sm:h-full w-full">
+                <img
+                  alt="image"
+                  src="/images/provider.jpg"
+                  className="w-full h-36 lg:h-full lg:rounded-l-lg lg:rounded-r-none rounded-t-lg object-cover"
+                />
+              </div>
+              <div className="text-sm sm:text-xl lg:text-sm xl:text-xl pl-3 pr-1 sm:pl-5 sm:pr-5 text-left flex flex-col lg:pb-8 pb-0 ">
+                <span className="text-lg md:text-3xl sm:text-2xl lg:text-xl 2xl:text-2xl mb-3 2xl:mb-0 pt-4 sm:pt-6 lg:pt-8 xl:pt-12 2xl:pt-8 md:pb-8 pb-0 2xl:pb-4 xl:pb-8 lg:pb-6 text-center xl:px-0 px-10">
+                  For Caregivers
                 </span>
-                <span className="grid grid-flow-col auto-cols-auto">
+                <span className="grid grid-flow-col auto-cols-auto mt-6 sm:mt-5 sm:mb-0 mb-2">
                   <img
                     src="/images/bricks.png"
                     className="h-6 w-6 ml-1 mr-4 mt-3"
@@ -105,7 +116,7 @@ class Features extends React.Component {
                   <span className="pr-1">Standard government inspection</span>
                   <br></br>
                 </span>
-                <span className="grid grid-flow-col auto-cols-auto">
+                <span className="grid grid-flow-col auto-cols-auto sm:mb-0 mb-2">
                   <img
                     src="/images/bricks.png"
                     className="h-6 w-6 ml-1 mr-4 mt-3"
@@ -115,7 +126,7 @@ class Features extends React.Component {
                   </span>
                   <br></br>
                 </span>
-                <span className="grid grid-flow-col auto-cols-auto">
+                <span className="grid grid-flow-col auto-cols-auto sm:mb-0 mb-2">
                   <img
                     src="/images/bricks.png"
                     className="h-6 w-6 ml-1 mr-4 mt-3"
@@ -123,7 +134,7 @@ class Features extends React.Component {
                   <span className="pr-1">Daily, weekly, or monthly plans</span>
                   <br></br>
                 </span>
-                <span className="grid grid-flow-col auto-cols-auto">
+                <span className="grid grid-flow-col auto-cols-auto sm:mb-0 mb-2">
                   <img
                     src="/images/bricks.png"
                     className="h-6 w-6 ml-1 mt-3"
@@ -133,7 +144,7 @@ class Features extends React.Component {
                   </span>
                   <br></br>
                 </span>
-                <span className="grid grid-flow-col auto-cols-auto">
+                <span className="grid grid-flow-col auto-cols-auto sm:mb-0 mb-2">
                   <img
                     src="/images/bricks.png"
                     className="h-6 w-6 ml-1 mt-1"
@@ -141,7 +152,7 @@ class Features extends React.Component {
                   <span className="pr-6 pl-2">In-built messaging tool</span>
                   <br></br>
                 </span>
-                <span className="grid grid-flow-col auto-cols-auto">
+                <span className="grid grid-flow-col auto-cols-auto sm:mb-0 mb-2">
                   <img
                     src="/images/bricks.png"
                     className="h-6 w-6 ml-1 mt-3"

a5492c20a98bee8580ad68bed9a6b51dad56e3e9
Anujj <anujjbubna39@gmail.com>
2024-05-11 23:47:32 -0700

    made features responsive for 2xl

--- src/views/components/features.js
+++ src/views/components/features.js
@@ -1,98 +1,157 @@
 import React from "react";
 
 class Features extends React.Component {
-    render() {
-        return (
-            <div id="features" className="w-full h-screen bg-baby-blue snap-start shrink-0 snap-always pb-48">
-                <div className="h-full w-full pt-24">
-                    <h1 className="text-7xl text-center pb-8 text-white drop-shadow-xl">Perks we offer</h1>
-                    <p className="w-full pt-4 text-center italic drop-shadow-xl text-white">Other than chocolates, of course :P</p>
-                    <div className="grid justify-items-center grid-cols-2 auto-cols-auto h-3/5 ml-8 mr-14 mt-16">
-                        <div className="grid justify-items-center grid-cols-2 ml-8 mr-4 rounded-xl text-slate-600
+  render() {
+    return (
+      <div
+        id="features"
+        className="box-border w-screen h-screen text-white bg-baby-blue snap-start shrink-0 snap-always"
+      >
+        <div className="w-full h-full lg:pt-28 pt-20">
+          <h1 className="text-4xl lg:text-5xl xl:text-6xl text-center lg:pb-8 pb-6 2xl:pb-2 text-white drop-shadow-xl lg:pt-0 px-4">
+            Perks we offer
+          </h1>
+          <p className="w-full pt-0 lg:pt-2 xl:pt-4 2xl:pt-2 px-2 text-center italic drop-shadow-xl text-white lg:text-base text-sm">
+            Other than chocolates, of course :P
+          </p>
+          <div className="grid justify-items-center grid-cols-2 auto-cols-auto ml-8 mr-14 pt-4 xl:pt-10 h-3/5">
+            <div
+              className="grid justify-items-center grid-cols-2 ml-8 mr-4 rounded-xl text-slate-600
                                        border-2 border-slate-400 bg-slate-100 drop-shadow-md
-                                       hover:drop-shadow-xl">
-                            <img
-                                alt="image"
-                                src="/images/parentchild.jpg"
-                                className="w-full h-full rounded-l-lg"
-                            />
-                            <div className="text-xl px-5 text-left flex flex-col space-y-4 pb-8">
-                                <span className="text-3xl mb-3 pt-12 pb-8 text-center">For Parents & Children </span>
-                                <span className="grid grid-flow-col auto-cols-auto">
-                                    <img src="/images/bricks.png" className="h-6 w-6 ml-1 mr-4 mt-3"></img> 
-                                    <span className="">Standard government inspection for caregivers</span>
-                                    <br></br>
-                                </span>
-                                <span className="grid grid-flow-col auto-cols-auto">
-                                    <img src="/images/bricks.png" className="h-6 w-6 ml-1 mr-4 mt-1"></img> 
-                                    <span className="pr-1">Meet the caregiver before</span>
-                                    <br></br>
-                                </span>
-                                <span className="grid grid-flow-col auto-cols-auto">
-                                    <img src="/images/bricks.png" className="h-6 w-6 ml-1 mr-4 mt-3"></img> 
-                                    <span className="pr-1">Filters include location, price, and ratings </span>
-                                    <br></br>
-                                </span>
-                                <span className="grid grid-flow-col auto-cols-auto">
-                                    <img src="/images/bricks.png" className="h-6 w-6 ml-1 mr-4 mt-3"></img> 
-                                    <span className="pr-1">Seemless appointment booking</span>
-                                    <br></br>
-                                </span>
-                                <span className="grid grid-flow-col auto-cols-auto">
-                                    <img src="/images/bricks.png" className="h-6 w-6 ml-1 mr-4 mt-3"></img> 
-                                    <span className="pr-1">Daily, weekly, or monthly plans</span>
-                                    <br></br>
-                                </span>
-                                <span className="grid grid-flow-col auto-cols-auto">
-                                    <img src="/images/bricks.png" className="h-6 w-6 ml-1 mr-4 mt-3"></img> 
-                                    <span className="pr-1">Integrated payments system</span>
-                                    <br></br>
-                                </span>
-                            </div>
-                        </div>
-                        <div className="grid justify-items-center grid-cols-2 ml-4 mr-4 rounded-xl text-slate-600
-                                        border-2 border-slate-400 bg-slate-100 drop-shadow-md
-                                        hover:drop-shadow-xl">
-                            <img
-                                alt="image"
-                                src="/images/provider.jpg"
-                                className="w-full h-full rounded-l-lg object-cover"
-                            />
-                            <div className="text-xl px-5 pr-10 flex flex-col space-y-5 pt-5 pb-8">
-                                <span className="text-3xl text-center mb-3 pb-6 pt-12">For Caregiver</span>
-                                <span className="grid grid-flow-col auto-cols-auto">
-                                    <img src="/images/bricks.png" className="h-6 w-6 ml-1 mr-4 mt-3"></img> 
-                                    <span className="pr-1">Standard government inspection</span>
-                                    <br></br>
-                                </span>
-                                <span className="grid grid-flow-col auto-cols-auto">
-                                    <img src="/images/bricks.png" className="h-6 w-6 ml-1 mr-4 mt-3"></img> 
-                                    <span className="pr-1">Onboarding & registration support</span>
-                                    <br></br>
-                                </span>
-                                <span className="grid grid-flow-col auto-cols-auto">
-                                    <img src="/images/bricks.png" className="h-6 w-6 ml-1 mr-4 mt-3"></img> 
-                                    <span className="pr-1">Daily, weekly, or monthly plans</span>
-                                    <br></br>
-                                </span>
-                                <span className="grid grid-flow-col auto-cols-auto">
-                                    <img src="/images/bricks.png" className="h-6 w-6 ml-1 mt-3"></img> 
-                                    <span className="pr-1 pl-4">Calendar and Income management</span>
-                                    <br></br>
-                                </span>
-                                <span className="grid grid-flow-col auto-cols-auto">
-                                    <img src="/images/bricks.png" className="h-6 w-6 ml-1 mt-1"></img> 
-                                    <span className="pr-6 pl-2">In-built messaging tool</span>
-                                    <br></br>
-                                </span>
-                                <span className="grid grid-flow-col auto-cols-auto">
-                                    <img src="/images/bricks.png" className="h-6 w-6 ml-1 mt-3"></img> 
-                                    <span className="pr-1 pl-4">Integrated payment system</span>
-                                    <br></br>
-                                </span>
-                            </div>
-                        </div>
-                        {/* <div className="home-feature-card2">
+                                       hover:drop-shadow-xl h-5/6"
+            >
+              <img
+                alt="image"
+                src="/images/parentchild.jpg"
+                className="w-full h-5/6 rounded-l-lg object-cover"
+              />
+              <div className="text-xl 2xl:text-xl px-5 text-left flex flex-col pb-8">
+                <span className="text-3xl 2xl:text-2xl mb-3 2xl:mb-0 pt-12 2xl:pt-8 pb-8 2xl:pb-4 text-center">
+                  For Parents & Children
+                </span>
+                <span className="grid grid-flow-col auto-cols-auto">
+                  <img
+                    src="/images/bricks.png"
+                    className="h-6 w-6 ml-1 mr-4 mt-3"
+                  ></img>
+                  <span className="">
+                    Standard government inspection for caregivers
+                  </span>
+                  <br></br>
+                </span>
+                <span className="grid grid-flow-col auto-cols-auto">
+                  <img
+                    src="/images/bricks.png"
+                    className="h-6 w-6 ml-1 mr-4 mt-1"
+                  ></img>
+                  <span className="pr-1">Meet the caregiver before</span>
+                  <br></br>
+                </span>
+                <span className="grid grid-flow-col auto-cols-auto">
+                  <img
+                    src="/images/bricks.png"
+                    className="h-6 w-6 ml-1 mr-4 mt-3"
+                  ></img>
+                  <span className="pr-1">
+                    Filters include location, price, and ratings{" "}
+                  </span>
+                  <br></br>
+                </span>
+                <span className="grid grid-flow-col auto-cols-auto">
+                  <img
+                    src="/images/bricks.png"
+                    className="h-6 w-6 ml-1 mr-4 mt-3"
+                  ></img>
+                  <span className="pr-1">Seemless appointment booking</span>
+                  <br></br>
+                </span>
+                <span className="grid grid-flow-col auto-cols-auto">
+                  <img
+                    src="/images/bricks.png"
+                    className="h-6 w-6 ml-1 mr-4 mt-3"
+                  ></img>
+                  <span className="pr-1">Daily, weekly, or monthly plans</span>
+                  <br></br>
+                </span>
+                <span className="grid grid-flow-col auto-cols-auto">
+                  <img
+                    src="/images/bricks.png"
+                    className="h-6 w-6 ml-1 mr-4 mt-3"
+                  ></img>
+                  <span className="pr-1">Integrated payments system</span>
+                  <br></br>
+                </span>
+              </div>
+            </div>
+            <div
+              className="grid justify-items-center grid-cols-2 ml-8 mr-4 rounded-xl text-slate-600
+                                       border-2 border-slate-400 bg-slate-100 drop-shadow-md
+                                       hover:drop-shadow-xl h-5/6"
+            >
+              <img
+                alt="image"
+                src="/images/provider.jpg"
+                className="w-full h-full rounded-l-lg object-cover"
+              />
+              <div className="text-xl 2xl:text-xl px-5 text-left flex flex-col pb-8">
+                <span className="text-3xl 2xl:text-2xl mb-3 2xl:mb-0 pt-12 2xl:pt-8 pb-8 2xl:pb-4 text-center">
+                  For Caregiver
+                </span>
+                <span className="grid grid-flow-col auto-cols-auto">
+                  <img
+                    src="/images/bricks.png"
+                    className="h-6 w-6 ml-1 mr-4 mt-3"
+                  ></img>
+                  <span className="pr-1">Standard government inspection</span>
+                  <br></br>
+                </span>
+                <span className="grid grid-flow-col auto-cols-auto">
+                  <img
+                    src="/images/bricks.png"
+                    className="h-6 w-6 ml-1 mr-4 mt-3"
+                  ></img>
+                  <span className="pr-1">
+                    Onboarding & registration support
+                  </span>
+                  <br></br>
+                </span>
+                <span className="grid grid-flow-col auto-cols-auto">
+                  <img
+                    src="/images/bricks.png"
+                    className="h-6 w-6 ml-1 mr-4 mt-3"
+                  ></img>
+                  <span className="pr-1">Daily, weekly, or monthly plans</span>
+                  <br></br>
+                </span>
+                <span className="grid grid-flow-col auto-cols-auto">
+                  <img
+                    src="/images/bricks.png"
+                    className="h-6 w-6 ml-1 mt-3"
+                  ></img>
+                  <span className="pr-1 pl-4">
+                    Calendar and Income management
+                  </span>
+                  <br></br>
+                </span>
+                <span className="grid grid-flow-col auto-cols-auto">
+                  <img
+                    src="/images/bricks.png"
+                    className="h-6 w-6 ml-1 mt-1"
+                  ></img>
+                  <span className="pr-6 pl-2">In-built messaging tool</span>
+                  <br></br>
+                </span>
+                <span className="grid grid-flow-col auto-cols-auto">
+                  <img
+                    src="/images/bricks.png"
+                    className="h-6 w-6 ml-1 mt-3"
+                  ></img>
+                  <span className="pr-1 pl-4">Integrated payment system</span>
+                  <br></br>
+                </span>
+              </div>
+            </div>
+            {/* <div className="home-feature-card2">
                         <img
                             alt="image"
                             src="https://images.unsplash.com/photo-1619555241737-9c364cf1fbce?ixlib=rb-1.2.1&amp;q=85&amp;fm=jpg&amp;crop=entropy&amp;cs=srgb&amp;h=1000"
@@ -105,11 +164,11 @@ class Features extends React.Component {
                             nisi. Nulla quis sem at nibh elementum imperdiet.
                         </span>
                     </div> */}
-                    </div>
-                </div>
-            </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Features;
--- src/views/components/product.js
+++ src/views/components/product.js
@@ -5,20 +5,20 @@ class Product extends React.Component {
         return (
             <div id="product" className="box-border w-screen h-screen text-white bg-baby-pink snap-start shrink-0 snap-always">
                 <div className="w-full h-full lg:pt-28 pt-20">
-                    <h1 className="text-4xl lg:text-5xl xl:text-6xl text-center lg:pb-8 pb-6 text-slate-700 drop-shadow-xl lg:pt-0 px-4">So what is BabysDayOut?</h1>
-                    <p className="w-full pt-0 lg:pt-2 xl:pt-4 px-2 text-center italic drop-shadow-xl text-slate-800 lg:text-base text-sm">It is the future of daycare... and even the children.</p>
+                    <h1 className="text-4xl lg:text-5xl xl:text-6xl text-center lg:pb-8 pb-6 2xl:pb-2 text-slate-700 drop-shadow-xl lg:pt-0 px-4">So what is BabysDayOut?</h1>
+                    <p className="w-full pt-0 lg:pt-2 xl:pt-4 2xl:pt-2 px-2 text-center italic drop-shadow-xl text-slate-800 lg:text-base text-sm">It is the future of daycare... and even the children.</p>
                     <div className="grid lg:grid-cols-2 grid-rows-2 xl:mt-10 md:mt-6 mt-4 justify-items-center w-full bg-white drop-shadow-xl h-3/5">
-                        <p className="text-center xl:text-2xl lg:leading-loose xl:leading-extra-loose 2xl:leading-extra-loose md:leading-loose lg:pr-10 lg:pl-16 md:px-24 px-3 lg:w-4/5 text-slate-700 h-full
-                                     md:text-xl lg:text-lg 2xl:text-3xl leading-relaxed 2xl:mt-24 xl:mt-14 lg:mt-10 mt-8 md:mt-10">
+                        <p className="text-center xl:text-2xl lg:leading-loose xl:leading-extra-loose 2xl:leading-loose md:leading-loose lg:pr-10 lg:pl-16 md:px-24 px-3 lg:w-4/5 text-slate-700 h-full
+                                     md:text-xl lg:text-lg 2xl:text-2xl leading-relaxed 2xl:mt-8 xl:mt-14 lg:mt-10 mt-8 md:mt-10">
                             BabysDayOut is an opportunity to revolutionize the childcare system by utiziling
                             already available infrastructure.
                             Our digital platform connects parents with trusted, local caregivers
                             in the community willing to host and look after children in the
                             perimeter & safety of their own homes.
                         </p>
-                        <div className="text-slate-800 2xl:text-2xl xl:text-xl md:text-lg text-sm lg:space-y-24 xl:space-y-32
-                                        lg:py-14 xl:pt-16 xl:pb-24 py-4 grid grid-cols-2 lg:grid-cols-1 lg:grid-rows-2">
-                            <div className="w-3/4 mx-auto justify-items-center xl:space-y-5 md:space-y-2 space-y-5">
+                        <div className="text-slate-800 2xl:text-xl xl:text-xl md:text-lg text-sm lg:space-y-24 xl:space-y-32
+                                        lg:py-14 xl:pt-16 xl:pb-24 2xl:pb-32 2xl:pt-12 py-4 grid grid-cols-2 lg:grid-cols-1 lg:grid-rows-2">
+                            <div className="w-3/4 mx-auto justify-items-center xl:space-y-5 2xl:space-y-3 md:space-y-2 space-y-5">
                                 <div className="inline-block">
                                     <img src="/images/check-mark.png"
                                         className="w-4 float-left lg:py-2 py-1 mx-3" />
@@ -36,7 +36,7 @@ class Product extends React.Component {
                                 </div>
 
                             </div>
-                            <div className="w-3/4 mx-auto justify-items-center xl:space-y-5 md:space-y-2 xl:pt-8 space-y-5">
+                            <div className="w-3/4 mx-auto justify-items-center xl:space-y-5 2xl:space-y-3 md:space-y-2 xl:pt-8 2xl:pt-2 space-y-5">
                                 <div className="inline-block">
                                     <img src="/images/close.png"
                                         className="w-4 float-left lg:py-2 py-1 mx-3" />
@@ -55,7 +55,7 @@ class Product extends React.Component {
                             </div>
                         </div>
                     </div>
-                    <div className="flex justify-center w-3/10 2xl:pt-16 xl:pt-12 md:pt-20 lg:pt-8 pt-10
+                    <div className="flex justify-center w-3/10 2xl:pt-6 xl:pt-12 md:pt-20 lg:pt-8 pt-10
                                      text-base md:text-xl hover:text-2xl hover:ease-in-out duration-300 mr-10 md:mr-4 lg:mr-0">
                         <p className="text-center float-left pl-4 py-1 text-slate-700 font-medium drop-shadow-xl">Interested parents and caregivers, waitlist today!</p>
                         <a href="https://shorturl.at/lpzX1" target="_blank"><button className="bg-slate-700 float-right px-3 md:ml-4 ml-3 py-1 md:mt-0 mt-2
